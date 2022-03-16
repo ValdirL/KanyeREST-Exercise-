d473b08261553505e5b2878b77e9847cc4c9bf70
Valdir Lima <valdirlima508@gmail.com>
2022-03-16 13:10:45 -0400

    initial commit

---
+++ KanyeREST/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine($"Kanye: {quote.KanyeQuote()}");
+
+                Console.WriteLine($"Ron Swanson: {quote.RonQuote()}");
+            }
+        }
+    }
+}
---
+++ KanyeREST/QuoteGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeREST
+{
+    public class QuoteGenerator
+    {
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+        public string KanyeQuote()
+        {
+            //API URL
+            var kanyeURL = "https://api.kanye.rest ";
+            //Stores the JSON response in a variable
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            //This parses through the response we recieve to get the value associated
+            //with the name "quote"
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonQuote()
+        {
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+
+        }
+    }
+}
